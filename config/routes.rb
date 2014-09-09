6e657e3cdaa85c9eb2b9efb5f6252454ab8b9568
Deedee Chiang <DDChiang@users.noreply.github.com>
2014-09-09 09:54:25 -0700

    deleted sessions path in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   match '/contact', to: 'static_pages#contact', via: 'get'
   
-  match '/sign_in', to: 'new_user_session_path', via: 'post'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
